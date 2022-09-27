1e5d88cb3a182a8a1a43302575241f185e35c50a
danie <71136486+lajbel@users.noreply.github.com>
2022-09-27 15:26:40 +0000

    art source files

---
+++ README.md
@@ -0,0 +1,1 @@
+# 🏝️ lajbel.land
---
+++ config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            klee: "Klee One"
+        },
+        extend: {
+            cursor: {
+                'landcur': 'url("images/cursor.png"), default',
+                'landpo': 'url("images/pointer.png"), pointer',
+            }
+        }
+    },
+};
